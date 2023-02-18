9238005c640ff3f5176851617ac2969b5b60ec22
Shiv <shivg03@gmail.com>
2023-02-19 04:09:16 +0530

    v0.1

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
+global CITY
+CITY='Delhi'
 @app.route('/weather',methods=['GET','POST'])
 def hello_world():
-    CITY = 'Delhi'
-    if request.method == 'POST':
-        new_city = request.form.get('value')
-        CITY=new_city
-        print(CITY)
     API_KEY="637512c0e47523469e20ef76357f0eef"
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    global CITY
+    if request.method == 'POST':
+        new_city = request.get_json(force=True)
+        print(new_city)
+        print(type(new_city))
+        a=new_city['title']
+        CITY=a
+        print(CITY)
     URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
     response = requests.get(URL)
     data = response.json()
