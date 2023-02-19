875fc643bf4284e894866a70f0a6b765438a7943
Shiv <shivg03@gmail.com>
2023-02-19 14:02:55 +0530

    fixed glitch
    added 0.01s delay in get to let post finish updating

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 from flask_cors import CORS
+import time
 app = Flask(__name__)
 CORS(app)
 global CITY
@@ -17,17 +18,21 @@ def hello_world():
         a=new_city['title']
         CITY=a
         print(CITY)
-    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
-    response = requests.get(URL)
-    data = response.json()
-    weather1={
-        'city':CITY,
-        'temperature':round(data['main']['temp']-273.15),
-        'humidity':data['main']['humidity'],
-        'description':data['weather'][0]['description'],
-        'icon':data['weather'][0]['icon']
-    }
-    return weather1
 
+    if request.method == 'GET':
+        time.sleep(0.01)
+        URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+        response = requests.get(URL)
+        data = response.json()
+        weather1={
+            'city':CITY,
+            'temperature':round(data['main']['temp']-273.15),
+            'humidity':data['main']['humidity'],
+            'description':data['weather'][0]['description'],
+            'icon':data['weather'][0]['icon']
+        }
+        print(weather1)
+        return weather1
+    return 'Hello, World!'
 if __name__ == '__main__':
     app.run(debug=True)
